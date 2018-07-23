c21a7e5fdc1b0d160f5adb09d034a824e9c3b40a
Shanjaq <shanjaq@gmail.com>
2018-07-23 00:19:11 -0700

    fix whitespace and actually use PSDrive

--- websrv.ps1
+++ websrv.ps1
@@ -1,4 +1,5 @@
 #run this first: netsh http add urlacl url=http://+:8000/ user=DOMAIN\user
+Add-Type -AssemblyName System.Web
 $Hso = New-Object Net.HttpListener
 $Hso.Prefixes.Add("http://+:8000/")
 $Hso.Start()
@@ -9,35 +10,37 @@ While ($Hso.IsListening) {
     $HC = $Hso.GetContext()
     $HReq = $HC.Request
     $HRes = $HC.Response
-	$fs = New-Object System.IO.FileStream((Get-Item ((Join-Path (Join-Path $Pwd "http") ($HC.Request).RawUrl))), [System.IO.FileMode]::Open)
 	
-	$readStart = 0
-	if ("Range" -in $HReq.Headers.AllKeys)
-	{
-		$readStart = [Int64]($HReq.Headers["Range"] -replace "^bytes=([0-9]+).*","`$1")
-		$fs.Seek($readStart, [System.IO.SeekOrigin]::Begin) | Out-Null
-		$HRes.Headers.Add("Content-Range","bytes $($readStart)-$($fs.Length-1)/$($fs.Length)")
-		$HRes.StatusCode = 206;
-	}
+    $localPath = Join-Path (Join-Path "MyPowerShellSite:" "http") ($HC.Request).RawUrl
+    $fs = New-Object System.IO.FileStream((Get-Item $localPath), [System.IO.FileMode]::Open)
+    $HRes.ContentType = [System.Web.MimeMapping]::GetMimeMapping($localPath)
 
-	$HRes.Headers.Add("Content-Type","application/octet-stream")
-	$HRes.ContentLength64 = $fs.Length - $readStart
-	$readLen = $fs.Read($Buf, 0, $BufSize)
-	
-	Try
-	{
-		While ($readLen -gt 0)
-		{
-			$HRes.OutputStream.Write($Buf,0,$readLen)
-			$readLen = $fs.Read($Buf, 0, $BufSize)
-			$readStart += $readLen
-		}
-	}
-	Catch
-	{
-		Write-Host $_.Exception.Message
-	}
-	$fs.Close()
+    $readStart = 0
+    if ("Range" -in $HReq.Headers.AllKeys)
+    {
+        $readStart = [Int64]($HReq.Headers["Range"] -replace "^bytes=([0-9]+).*","`$1")
+        $fs.Seek($readStart, [System.IO.SeekOrigin]::Begin) | Out-Null
+        $HRes.Headers.Add("Content-Range","bytes $($readStart)-$($fs.Length-1)/$($fs.Length)")
+        $HRes.StatusCode = 206
+    }
+
+    $HRes.ContentLength64 = $fs.Length - $readStart
+
+    Try
+    {
+        $readLen = 0
+        Do
+        {
+            $readLen = $fs.Read($Buf, 0, $BufSize)
+            $HRes.OutputStream.Write($Buf,0,$readLen)
+        } 
+        While ($readLen -gt 0)
+    }
+    Catch
+    {
+        Write-Host $_.Exception.Message
+    }
+    $fs.Close()
     $HRes.Close()
 }
 $Hso.Stop()
